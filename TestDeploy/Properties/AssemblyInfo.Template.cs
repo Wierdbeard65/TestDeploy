1026feb40b1e8a39946623d10212d843569cfb54
Wierdbeard65 <paul@maggiandpaul.com>
2016-01-06 07:33:56 -0500

    Testing new footer (1)

--- TestDeploy/Properties/AssemblyInfo.Template.cs
+++ TestDeploy/Properties/AssemblyInfo.Template.cs
@@ -6,8 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestDeploy")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("Build started: {MM}/{DD}/{YYYY} at {hh}:{mm}")]
+[assembly: AssemblyDescription("Build started: {MM}/{DD}/{YYYY} at {hh}:{mm}")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TestDeploy")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
---
+++ TestDeploy/Views/HtmlHelperExtensions.cs
@@ -0,0 +1,48 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="HtmlHelperExtensions.cs" company="Interactive Intelligence">
+//     Copyright (c) Interactive Intelligence. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+/// <summary>
+/// Author: Paul Simpson
+/// </summary>
+namespace TestDeploy.Views
+{
+    using System.Linq;
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Sundry extensions to shove stuff into pages....
+    /// </summary>
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Get the Current Version from the AssemblyInfo.cs file.
+        /// </summary>
+        /// <param name="helper">The helper.?!</param>
+        /// <returns>The current version</returns>
+        public static MvcHtmlString CurrentVersion(this HtmlHelper helper)
+        {
+            try
+            {
+                string response;
+                var version = Assembly.GetExecutingAssembly().GetName().Version;
+                response = version.ToString();
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                object[] attributes = assembly.GetCustomAttributes(true);
+                var config = attributes.OfType<AssemblyConfigurationAttribute>().FirstOrDefault();
+                if (config != null)
+                {
+                    response = response +" "+ config.Configuration;
+                }
+                return MvcHtmlString.Create(response);
+            }
+            catch
+            {
+                return MvcHtmlString.Create("?.?.?.?");
+            }
+        }
+    }
+}
